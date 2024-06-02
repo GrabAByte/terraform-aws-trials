c1e4ead0113cd22737e30643be226c3de80a83de
Tom Richards <twr.devops@gmail.com>
2024-06-02 22:48:56 +0100

    feat: encorporate terraform validate job into workflow

--- environments/development/provider.tf
+++ environments/development/provider.tf
@@ -1,6 +1,7 @@
 provider aws {
-  region     = "eu-west-1"
-  sts_region = "eu-west-1"
+  shared_credentials_files = ['.aws_credentials']
+  region                   = "eu-west-1"
+  sts_region               = "eu-west-1"
   endpoints {
     sts = "https://sts.eu-west-1.amazonaws.com"
   }
