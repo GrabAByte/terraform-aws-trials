3c674f06915dbfa51b369013b53f495e8a5bbd6a
Tom Richards <twr.devops@gmail.com>
2024-06-03 09:07:16 +0100

    feat: encorporate terraform validate job into workflow

--- environments/pre-production/provider.tf
+++ environments/pre-production/provider.tf
@@ -1,12 +1,6 @@
 provider aws {
-  #profile                    = "default"
-  #shared_credentials_files   = [".aws_credentials"]
   region                      = "eu-west-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
-  #sts_region                 = "eu-west-1"
-  #endpoints {
-  #  sts = "https://sts.eu-west-1.amazonaws.com"
-  #}
 }
